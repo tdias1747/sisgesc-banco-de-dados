33123ee20be3a6e09b8fa4a11a3f25e7c3ba2356
tdias1747 <tdias1747@gmail.com>
2026-04-24 17:37:51 -0300

    sisgesc.sql

--- sisgesc.sql
+++ sisgesc.sql
@@ -17,7 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE sisgesc;
+CREATE DATABASE IF NOT EXISTS sisgesc;
+USE sisgesc;
 -- Banco de dados: `sisgesc`
 --
 
