17490434dfc26b20e6e7f6596f174d3c28193886
tdias1747 <tdias1747@gmail.com>
2026-04-25 12:35:38 -0300

    sisgesc_script_oltp.sql

--- sisgesc_script_oltp.sql
+++ sisgesc_script_oltp.sql
@@ -1,27 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25/04/2026 às 17:29
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `sisgesc`
---
 
--- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `dim_aluno`
