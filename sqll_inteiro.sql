211fe5ff4ebcd049adc9f62cd73a09096a26d164
tdias1747 <tdias1747@gmail.com>
2026-04-25 16:36:38 -0300

    sqll_inteiro.sql

--- sqll_inteiro.sql
+++ sqll_inteiro.sql
@@ -1,26 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25/04/2026 às 21:30
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `sisgesc`
 --
-
+CREATE IF EXISTS DATABASE sisgesc;
+USE sisgesc;
 -- --------------------------------------------------------
 
 --
@@ -611,6 +601,3 @@ ALTER TABLE `tb_turma`
   ADD CONSTRAINT `tb_turma_ibfk_2` FOREIGN KEY (`fk_curso`) REFERENCES `tb_curso` (`pk_curso`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
